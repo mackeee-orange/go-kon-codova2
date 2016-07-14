95297bea8aa04645dff4090ea455a2885f2677f2
Akihiro Makino <makino.aaa@gmail.com>
2016-07-14 15:25:02 +0900

    npmクリアにするよ

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var babelify = require('babelify');
+'use strict';
+
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const babelify = require('babelify');
+const reactify = require('reactify');
 
 
 gulp.task('browserify', function(){
-  browserify('./www/jsx/app.jsx', {debug:true})
-    .transform(babelify)
+  browserify('./www/jsx/app.jsx', {
+    entry:true,
+    extentions:[
+      'jsx',
+      'json',
+      'js',
+      'es6'
+    ],
+    debug:true
+  })
+    .transform(babelify, {"presets": [ "react", "es2015" ] })
     .bundle()
-    .on('error', function(error){console.log("Error:"+error.message);})
+    .on('error', function(error){
+      console.log("Error:"+error.message);
+      console.log("ES:"+error.stack);
+    })
     .pipe(source('app.js'))
     .pipe(gulp.dest('./www/js/'));
 });
--- www/jsx/app.jsx
+++ www/jsx/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+
 /*
 import {HomePage} from 'components/home.jsx';
 import {RoomPage} from 'components/room.jsx';
@@ -8,15 +9,19 @@ import {ChatPage} from 'components/chat.jsx';
 import {LovePage} from 'components/love.jsx';
 import {Header} from 'components/header.jsx';
 */
-var HomePage = require('./components/home.jsx');
-var RoomPage = require('./components/room.jsx');
-var ChatPage = require('./components/chat.jsx');
-var LovePage = require('./components/love.jsx');
-var Header = require('./components/header.jsx');
+const HomePage = require('./components/home.jsx');
+const RoomPage = require('./components/room.jsx');
+const ChatPage = require('./components/chat.jsx');
+const LovePage = require('./components/love.jsx');
+const Header = require('./components/header.jsx');
 
 
-var App = React.createClass({
-  render: function() {
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
       <div className="app">
         <Header />
@@ -26,9 +31,9 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
 
-var routes = (
+const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route component={HomePage}/>
@@ -38,4 +43,15 @@ var routes = (
   </Route>
 );
 
-ReactDOM.render(<Route history={browserHistory}>{routes}</Route>, document.getElementById('app'));
+render(
+  (<Route history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage}/>
+      <Route component={HomePage}/>
+      <Route component={RoomPage}/>
+      <Route component={ChatPage}/>
+      <Route component={LovePage}/>
+    </Route>
+  </Route>), 
+  document.getElementById('app')
+);
